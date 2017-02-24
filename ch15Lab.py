e429fac026fb66e92c77b2c9cb1e9108cb7bc360
bernhardtjj <jbernhardt@fwparker.org>
2017-02-24 14:57:50 -0600

    The Homework Done
    Completed, Finished, etc. et al.

--- ch15Lab.py
+++ ch15Lab.py
@@ -1,6 +1,51 @@
-'''
+"""
 Complete the chapter lab at http://programarcadegames.com/index.php?chapter=lab_spell_check
-'''
+"""
+
+import re
 
 
+# This function takes in a file of text and returns
+# a list of words in the file.
+def split_file(file):
+    return [x for line in file for x in re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line.strip())]
 
+
+dict_words = split_file(open("dictionary.txt"))
+
+
+def split_file(file):  # this returns it in lines
+    return [re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line.strip()) for line in file]
+
+
+alice = split_file(open("AliceInWonderLand200.txt"))
+
+print("--- Linear Search ---")
+
+for n in range(len(alice)):
+    for word in alice[n]:
+        i = 0
+        while i < len(dict_words) and dict_words[i] != word.upper():
+            i += 1
+
+        if i >= len(dict_words):
+            print("Line", n + 1, "possible misspelled word:", word)
+
+print("--- Binary Search ---")
+
+for i in range(len(alice)):
+    for word in alice[i]:
+        lower_bound = 0
+        upper_bound = len(dict_words) - 1
+        found = False
+        while lower_bound <= upper_bound and not found:
+            target = (lower_bound + upper_bound) // 2
+            if dict_words[target] < word.upper():
+                lower_bound = target + 1
+            elif dict_words[target] > word.upper():
+                upper_bound = target - 1
+            else:
+                found = True
+
+        if not found:
+            print("Line", i + 1, "possible misspelled word:", word)
--- ch15ProblemSet.py
+++ ch15ProblemSet.py
@@ -1,28 +1,106 @@
-'''
+"""
 Complete the following 3 searching problems using techniques
 from class and from Ch15 of the textbook website
-'''
+"""
+import re
 
-#1.  (7pts) Write code which finds and prints the longest
+
+# This function takes in a line of text and returns
+# a list of words in the line.
+def split_line(lin):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', lin)
+
+
+# 1.  (7pts) Write code which finds and prints the longest
 # word in the provided dictionary.  If there are more
 # than one longest word, print them all.
 
-#2.  (10pts)  Write code which finds
+def find_longest(file):
+    high_score = 0
+    saved_words = []
+    for line in file:
+        line = line.strip()
+        for word in split_line(line):
+            if len(word) >= high_score:
+                high_score = len(word)
+                saved_words.append(word)
+    file.close()
+    print([i for i in saved_words if len(i) == high_score])
+
+
+find_longest(open("AliceInWonderLand.txt"))
+find_longest(open("dictionary.txt"))
+
+
+# 2.  (10pts)  Write code which finds
 # The total word count AND average word length
 # in "AliceInWonderLand.txt"
+def find_average_and_word_count(file):
+    saved_words = []
+    for line in file:
+        line = line.strip()
+        for word in split_line(line):
+            saved_words.append(len(word))
+    file.close()
+    print("The word count is", len(saved_words), "and the avg word length is", sum(saved_words) / len(saved_words))
+
+
+find_average_and_word_count(open("AliceInWonderLand.txt"))
+
 
 # CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS
 
-#3 (13pts)  How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
+# 3 (13pts)  How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
 # How many times does "Cat" occur?
 # How many times does "Cheshire" immediately followed by "Cat" occur?
 
-#### OR #####
+def find_num_cat(file):
+    high_score = "cheshire"
+    low_score = "cat"
+    chess_score = 0
+    cat_score = 0
+    both_score = 0
+    last_word = ""
+    for line in file:
+        line = line.strip()
+        for word in split_line(line):
+            if word.lower() == high_score:
+                chess_score += 1
+            elif word.lower() == low_score:
+                cat_score += 1
+                if last_word == high_score:
+                    both_score += 1
+            last_word = word.lower()
+    file.close()
+    print("Cheshire is", chess_score, "times and Cat is", cat_score, "times and Cheshire Cat is", both_score, "times.")
 
-#3  (13pts)Find the most frequently occurring
+
+# OR
+
+# 3  (13pts)Find the most frequently occurring
 # seven letter word in "AliceInWonderLand.txt"
 
-# Challenge problem (for fun).  What words appear in the text of "Alice in Wonderland" that DO NOT occur in "Alice Through the Looking Glass".  Make a list.  You can substitute this for any of the above problems.
+def find_frq(file):
+    high_score = 7
+    saved_words = []
+    for line in file:
+        line = line.strip()
+        for word in split_line(line):
+            if len(word) == high_score:
+                saved_words.append(word)
+    file.close()
+
+    while len(saved_words) - 1:
+        saved_words = [word for pos, word in enumerate(saved_words) if word in saved_words[:pos]]
+
+    print("The most frequently occuring seven letter word is ``" + saved_words[0] + "''")
 
 
+find_num_cat(open("AliceInWonderLand.txt"))
+find_frq(open("AliceInWonderLand.txt"))
 
+# Challenge problem (for fun).
+# What words appear in the text of "Alice in Wonderland"
+# that DO NOT occur in "Alice Through the Looking Glass".  Make a list.
+# You can substitute this for any of the above problems.
+# Read in a file from disk and put it in an array.
